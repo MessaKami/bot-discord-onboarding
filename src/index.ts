670cd09fe3998b8c758dd81c60ba0ea1706930c6
Messa <messaoud.houri@outlook.fr>
2025-03-02 14:43:05 +0100

    fix(86c298zq8): pass client to interaction handler constructor

--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,6 @@ const client = new Client({
         GatewayIntentBits.Guilds,
         GatewayIntentBits.GuildMessages,
         GatewayIntentBits.GuildMembers,
-        GatewayIntentBits.MessageContent,
-        GatewayIntentBits.DirectMessages,
-        GatewayIntentBits.GuildVoiceStates
     ]
 });
 
